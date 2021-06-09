e5f7bd1bd277bde3bf349573a6c91011c2454ee8
Brooke Chalmers <breq@breq.dev>
2021-06-08 22:30:55 -0400

    Load genres from Pastebin

--- script.js
+++ script.js
@@ -3,7 +3,55 @@ Basically Hollywood is stupid.
     - Mitch LaFortune, 2021
 */
 
-import { genres } from "./genres.js"
+let genres = []
+
+const paste_url_input = document.getElementsByTagName("input").item(0)
+
+async function get_genres(pastebin_id) {
+    let pastebin_url = "https://pastebin-proxy.breq.workers.dev/?paste=" + pastebin_id
+
+    let response
+    response = await fetch(pastebin_url)
+
+    if (response.status != 200) {
+        return false
+    }
+
+    const data = await response.text()
+
+    genres = data.split("\n")
+    return true
+}
+
+async function handle_genre_change() {
+    const value = paste_url_input.value
+
+    const pastebin_url_regex = /^https:\/\/pastebin\.com\/(\w*)$/
+
+    const match = value.match(pastebin_url_regex)
+
+    if (match) {
+        if (await get_genres(match[1])) {
+            paste_url_input.classList.remove("invalid")
+
+            localStorage.setItem("pastebin_id", match[1])
+        } else {
+            paste_url_input.classList.add("invalid")
+        }
+    } else {
+        paste_url_input.classList.add("invalid")
+    }
+}
+
+let pastebin_id = localStorage.getItem("pastebin_id")
+if (pastebin_id === null) {
+    pastebin_id = "e3XYnWhT"
+}
+paste_url_input.value = "https://pastebin.com/" + pastebin_id
+
+handle_genre_change()
+paste_url_input.addEventListener("input", handle_genre_change)
+
 
 let history = []
 let position_in_history = -1;
@@ -28,11 +76,18 @@ function generate() {
 }
 
 document.addEventListener("click", (e) => {
+    if (e.target.tagName.toLowerCase() === "input") {
+        return
+    }
+
     generate()
     populate()
 })
 
 document.addEventListener("keydown", (e) => {
+    if (e.target.tagName.toLowerCase() === "input") {
+        return
+    }
 
     if (e.code === "Space") {
         generate()
